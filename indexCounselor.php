c2331d0b67ad69ce9f0dac97a6aa79c844530d22
Dasha <dashakd10@gmail.com>
2024-05-07 23:47:42 +0000

    adjusted the process

--- indexCounselor.php
+++ indexCounselor.php
@@ -11,74 +11,76 @@
 </head>
 <body>
 
-<div class="container" id="signup">
-    <h1 class="form-title">Register</h1>
-    <form method="post" action="register.php">
-        <div class="input-group">
-            <i class="fas fa-user"></i>
-            <input type="text" name="fName" id="fName" placeholder="First Name" required>
-            <label for="fname">First Name</label>
-        </div>
-        <div class="input-group">
-            <i class="fas fa-user"></i>
-            <input type="text" name="lName" id="lName" placeholder="Last Name" required>
-            <label for="lName">Last Name</label>
-        </div>
-        <div class="input-group">
-            <i class="fas fa-envelope"></i>
-            <input type="email" name="email" id="email" placeholder="Email" required>
-            <label for="email">Email</label>
-        </div>
-        <div class="input-group">
-            <i class="fas fa-lock"></i>
-            <input type="password" name="password" id="password" placeholder="Password" required>
-            <label for="password">Password</label>
-        </div>
-        <input type="submit" class="btn" value="Sign Up" name="signUp">
-    </form>
-    <p class="or">
-        ----------or----------
-    </p>
-    <div class="icons">
-        <i class="fab fa-google"></i>
-        <i class="fab fa-facebook-f"></i>
-    </div>
-    <div class="links">
-        <p>Already Have An Account ?</p>
-        <button id="signInButton">Sign In</button>
-    </div>
-</div>
-
-<div class="container" id="signIn" style="display:none;">
-    <h1 class="form-title">Sign In</h1>
-    <form method="post" action="register.php">
-        <div class="input-group">
-            <i class="fas fa-envelope"></i>
-            <input type="email" name="email" id="email" placeholder="Email" required>
-            <label for="email">Email</label>
-        </div>
-        <div class="input-group">
-            <i class="fas fa-lock"></i>
-            <input type="password" name="password" id="password" placeholder="Password" required>
-            <label for="password">Password</label>
-        </div>
-        <p class="recover">
-            <a href="#">Forgot Password ?</a>
+<main>
+    <div class="container" id="signup">
+        <h1 class="form-title">Register</h1>
+        <form method="post" action="register.php">
+            <div class="input-group">
+                <i class="fas fa-user"></i>
+                <input type="text" name="fName" id="fName" placeholder="First Name" required>
+                <label for="fname">First Name</label>
+            </div>
+            <div class="input-group">
+                <i class="fas fa-user"></i>
+                <input type="text" name="lName" id="lName" placeholder="Last Name" required>
+                <label for="lName">Last Name</label>
+            </div>
+            <div class="input-group">
+                <i class="fas fa-envelope"></i>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <label for="email">Email</label>
+            </div>
+            <div class="input-group">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <label for="password">Password</label>
+            </div>
+            <input type="submit" class="btn" value="Sign Up" name="signUp">
+        </form>
+        <p class="or">
+            ----------or----------
         </p>
-        <input type="submit" class="btn" value="Sign In" name="signIn">
-    </form>
-    <p class="or">
-        ----------or----------
-    </p>
-    <div class="icons">
-        <i class="fab fa-google"></i>
-        <i class="fab fa-facebook-f"></i>
+        <div class="icons">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook-f"></i>
+        </div>
+        <div class="links">
+            <p>Already Have An Account ?</p>
+            <button id="signInButton">Sign In</button>
+        </div>
     </div>
-    <div class="links">
-        <p>Don't have an account ?</p>
-        <button id="signUpButton">Sign Up</button>
+
+    <div class="container" id="signIn" style="display:none;">
+        <h1 class="form-title">Sign In</h1>
+        <form method="post" action="register.php">
+            <div class="input-group">
+                <i class="fas fa-envelope"></i>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+                <label for="email">Email</label>
+            </div>
+            <div class="input-group">
+                <i class="fas fa-lock"></i>
+                <input type="password" name="password" id="password" placeholder="Password" required>
+                <label for="password">Password</label>
+            </div>
+            <p class="recover">
+                <a href="#">Forgot Password ?</a>
+            </p>
+            <input type="submit" class="btn" value="Sign In" name="signIn">
+        </form>
+        <p class="or">
+            ----------or----------
+        </p>
+        <div class="icons">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook-f"></i>
+        </div>
+        <div class="links">
+            <p>Don't have an account ?</p>
+            <button id="signUpButton">Sign Up</button>
+        </div>
     </div>
-</div>
+</main>
 
 <!-- Footer -->
 <footer class="bg-gray-800 py-8">
