7ec16c4d04a159618f7852e1e805c705430fd26c
Dasha <dashakd10@gmail.com>
2024-05-09 02:49:36 +0000

    corrected src and href paths

--- indexCounselor.php
+++ indexCounselor.php
@@ -6,7 +6,7 @@
     <title>Counselor Access</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/styleUser.css">
+    <link rel="stylesheet" href="css/styleUser.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
@@ -87,7 +87,7 @@
     <div class="container mx-auto px-4">
         <div class="text-center">
             <a href="index.html" class="flex items-center text-black text-xl font-bold">
-                <img src="assets/img/currentAcityLogo.png" alt="Acity Counselling Service logo" class="h-8 mr-2">
+                <img src="img/currentAcityLogo.png" alt="Acity Counselling Service logo" class="h-8 mr-2">
                 Acity Counselling Service
             </a>
             <p class="text-black">&copy; 2024 Academic City University College Counselling Service. All rights reserved.</p>
@@ -95,8 +95,8 @@
     </div>
 </footer>
 
-<script src="assets/js/scriptUser.js"></script>
-<script src="assets/js/script.js"></script>
+<script src="js/scriptUser.js"></script>
+<script src="js/script.js"></script>
 
 </body>
 </html>
